6b37c6441b514004cf028f224949829d9692f04d
Manish Chand <manishchandr349@gmail.com>
2023-11-30 11:13:47 +0530

    Adding

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/index.html"],
+  content: ["./components/**/*.{js,ts,jsx,tsx}"],
   theme: {  
     extend: {},
   },
